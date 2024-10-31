3d38ea7221b5cf4d41480544aa99f77ed44a5555
Silva1997 <lucaseugeniofernando@gamil.com>
2024-10-31 20:21:11 +0000

    novo3

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,51 @@
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-import viteLogo from './assets/image.png'
+import React from "react";
 import "./Css/estilo.css";
+import Card from "antd/es/card/Card";
 import { MenuOutlined } from "@ant-design/icons";
 import Rolagem from "./Carousel/Carosel";
+import foto1 from './assets/image.png'
+
 function App() {
   return (
     <>
       <div className="Menu1">
-      <h6 id="texto3">Espelho.Meu</h6>
+        <h6 id="texto3">Espelho.Meu</h6>
         <button id="subMenu">
           <MenuOutlined size={20} className="icon" />
         </button>
       </div>
 
       <div className="Janela1">
-      <picture  className="posicao1" 
-      // style={{
-      //   backgroundImage: `url(${viteLogo })`,
-      // }}
-      >
-   
-        <div className="centralizarbutao">
-        <button id="butao1">Agendamento</button>
-        </div>
+        <picture className="posicao1">
+          <div className="centralizarbutao">
+            <button id="butao1">Agendamento</button>
+          </div>
           <h5 id="texto2">
             Por detrás de <br />
             cada cliente
           </h5>
-      </picture>
-       
+        </picture>
+      </div>
+
+      <div className="Janela2">
+        <div>
+          <Rolagem />
         </div>
-    
-      {/* <div>
-<Rolagem/>
-      </div> */}
+      </div>
+
+      <div className="Janela3">
+        <h6 id="texto1">Nossos Serviços</h6>
+        <div className="tagCard">
+         
+          <Card 
+          className="card">
+          <img src={foto1} className="img" alt="imagem1"/>
+          
+          </Card>
+          <Card className="card">2</Card>
+          <Card className="card">3</Card>
+        </div>
+      </div>
     </>
   );
 }
--- src/Carousel/Carosel.jsx
+++ src/Carousel/Carosel.jsx
@@ -1,74 +1,55 @@
-import React from 'react';
-import { Carousel,Card ,Image} from 'antd';
-import Cards from '../Cartao/Cards'
-import Imagem1 from '../assets/Na.jpg'
+import React from "react";
+import { Carousel, Card, Image } from "antd";
+import Cards from "../Cartao/Cards";
+import Imagem1 from "../assets/Na.jpg";
 const { Meta } = Card;
 const contentStyle = {
   margin: 0,
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
 };
 const Rolagem = () => {
   const onChange = (currentSlide) => {
     console.log(currentSlide);
   };
   return (
-   <>
-
-   <h5 id='texto1'>Nossos serviços</h5>
- <Carousel afterChange={onChange} 
- arrows
-//  autoplay
- style={{
-  height:'60vh',
-  // backgroundColor:'#364d79'
- }}>
- 
-      <div className='Card' >
-    <App/>
-    
-      </div>
-      <div className='Card' >
-    <App/>
-    
-      </div>
-      {/* <div>
-        <h3 style={contentStyle}>2</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>3</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>4</h3>
-      </div> */}
-    </Carousel>
-
-<div>
-    {/* <Cards/> */}
-</div>
-   </>
+    <>
+      <h6 id="texto1">Nossos Trabalhos</h6>
+      <Carousel
+        afterChange={onChange}
+        arrows
+        //  autoplay
+        style={{
+          height:"300px"
+        }}
+      >
+        <div className="Card">
+          <App />
+        </div>
+        <div className="Card">
+          <App />
+        </div>
+      </Carousel>
+    </>
   );
 };
 export default Rolagem;
 
-
-
-
-
-
-
 const App = () => (
   <Card
+  // bordered={false}
     hoverable
     style={{
-      width: 185,
-      margin:"1rem"
+    width:"200px",
+    height:"12px",
+    borderRadius:'12px 12px'
+      // margin:"1rem"
     }}
     cover={<img alt="example" src={Imagem1} />}
   >
-    <Meta title="Europe Street beat" description="www.instagram.com" />
+    {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
   </Card>
 );
